74e3f84ed5b627341d3351a1a37293fad1d4de41
playerunknownL <litheshkumar456@gmail.com>
2021-04-19 11:20:32 +0530

    yeah

---
+++ src/ok.js
@@ -0,0 +1,12 @@
+import React,{ Fragment} from "react";
+class Ok extends React.Component{
+    render (){
+        return(
+            <Fragment>
+        <h1>HI</h1>
+        <h2>welcome</h2>
+        </Fragment>
+        );
+    }
+}
+ export default Ok;
